42e88475f098826dd4fc3a15c85c124b54537748
codecalec <alexveltman0@gmail.com>
2019-12-24 17:38:01 +0200

    Initial skeleton

---
+++ react-client/src/Components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
