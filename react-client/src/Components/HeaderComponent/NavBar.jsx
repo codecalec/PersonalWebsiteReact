324eaca708e822633de5374882d9bb883d0bd527
codecalec <alexveltman0@gmail.com>
2019-12-29 20:05:03 +0200

    Minor changes

--- react-client/src/Components/HeaderComponent/NavBar.jsx
+++ react-client/src/Components/HeaderComponent/NavBar.jsx
@@ -4,16 +4,18 @@ import { NavLink } from "react-router-dom";
 class NavBar extends Component{
     render(){
         return (
-            <div className="header container">
-                <div class="row">
-                    <div className="one-third column">
-                        <NavLink exact to="/" className="nav">Home</NavLink>
-                    </div>
-                    <div class="one-third column">
-                        <NavLink to="/stuff" className="nav">Stuff</NavLink>
-                    </div>
-                    <div class="one-third column">
-                        <NavLink to="/contact" className="nav">Contact</NavLink>
+            <div className= "section header">
+                <div className="container">
+                    <div class="row">
+                        <div className="one-third column">
+                            <NavLink exact to="/" className="nav">Home</NavLink>
+                        </div>
+                        <div class="one-third column">
+                            <NavLink to="/stuff" className="nav">Stuff</NavLink>
+                        </div>
+                        <div class="one-third column">
+                            <NavLink to="/contact" className="nav">Contact</NavLink>
+                        </div>
                     </div>
                 </div>
             </div>
--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render() {
         return (
-            <div>
-                Home page
+            <div className="section content" style={{marginTop: "10%"}}>
+                <div className="container">
+                    <h1>About Me</h1>
+                    <text>
+                        My name is Alex Veltman and I'm an undergraduate student at UCT.
+                    </text>
+                </div>
             </div>
+
+            
         )
     }
 }
