95ebd47b092a93342565ed02a8fee264be8e36b8
codecalec <alexveltman0@gmail.com>
2019-12-31 18:16:31 +0200

    Got Footer in working order

--- react-client/src/Components/FooterComponent/Footer.jsx
+++ react-client/src/Components/FooterComponent/Footer.jsx
@@ -7,20 +7,38 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      icons: [faLinkedinIn, faGithub, faMediumM],
+      links: {
+        github: "https://github.com/codecalec",
+        "linkedin-in": "https://www.linkedin.com/in/alex-veltman-701aa2190"
+      },
+      color: "blue"
+    };
+  }
+
+  attachLink(name) {
+    window.location = this.state.links[name];
+  }
+
   render() {
     return (
       <div className="section footer">
         <div className="container">
           <div className="row">
-            <div className="one-third column">
-              <FontAwesomeIcon icon={faGithub} />
-            </div>
-            <div className="one-third column">
-              <FontAwesomeIcon icon={faLinkedinIn} />
-            </div>
-            <div className="one-third column">
-              <FontAwesomeIcon icon={faMediumM} />
-            </div>
+            {this.state.icons.map(icon => (
+              <div className="one-third column">
+                <FontAwesomeIcon
+                  className="nav footer"
+                  icon={icon}
+                  size="xl"
+                  onClick={() => this.attachLink(icon.iconName)}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
