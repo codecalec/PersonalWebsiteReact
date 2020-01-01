b64be3f33fd964e36bd9fb4a127f69b23e5acbb3
codecalec <alexveltman0@gmail.com>
2020-01-01 22:52:11 +0200

    Set up resume page

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const HomePage = lazy(() => import("./HomePage"));
 const StuffPage = lazy(() => import("./StuffPage"));
-const ResumePage = lazy(() => import("./ResumePage"))
+const ResumePage = lazy(() => import("./ResumePage"));
 
 import NavBar from "./HeaderComponent/NavBar";
 import Footer from "./FooterComponent/Footer";
--- webpack.config.js
+++ webpack.config.js
@@ -49,5 +49,5 @@ module.exports = {
   ],
   node: {
     fs: "empty"
- }
+  }
 };
